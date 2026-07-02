9699017736cb41d57d27a657ab24dd5276915f76
Pranshu Samadhiya <samadhiya.pran@gmail.com>
2026-07-02 22:41:59 +0530

    Routing

--- practice2/index.js
+++ practice2/index.js
@@ -1,9 +1,36 @@
+// const http = require("http");
+
+// const server = http.createServer((req,res) => {
+//         console.log(req.url);
+//     res.end("Done !!");
+
+
+// });
+
+// server.listen(3000, () => {
+   
+//     console.log("Server is running... ")
+// });
+
 const http = require("http");
 
 const server = http.createServer((req,res) => {
-    res.end("Hello Pranshu !!");
+
+    if(req.url === "/"){
+        res.end("Welcome to Home Page");
+    }
+    else if(req.url === "/about"){
+        res.end("Welcome to About Page");
+    }
+    else if(req.url === "/contact"){
+        res.end("Welcome to Contact Page");
+    }
+    else{
+        res.statusCode = 404;
+        res.end("Page Not Found");
+    }
 });
 
 server.listen(3000, () => {
-    console.log("Server is running... ")
+    console.log("Server Running... ");
 });
